64159ac0304408262b8c11193701cef7bd0db574
Everett <everett.bowline@gmail.com>
2021-04-26 13:34:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,16 +1,9 @@
 def position_taken?(board, index)
-  # basic solution:
-
-  # taken = nil
-  # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-  #   taken = false
-  # else
-  #   taken = true
-  # end
-  # taken
-
-  # advanced solution w/ ternary operator
-  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
-    false : true
-
+ taken = nil
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    taken = false
+  else
+    taken = true
+  end
+  taken
 end
